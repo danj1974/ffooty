096c549280a0a0fda467cadd4f507a860e6abc29
danj1974 <danj1974@gmail.com>
2017-11-02 00:11:13 +0000

    Openshift V3:  fix for app.sh

--- app.sh
+++ app.sh
@@ -6,4 +6,4 @@ ARGS="$ARGS --log-to-terminal"
 ARGS="$ARGS --port 8080"
 ARGS="$ARGS --url-alias /static wsgi/static"
 
-exec mod_wsgi-express start-server $ARGS wsgi/application
+exec mod_wsgi-express start-server $ARGS wsgi/application.py
