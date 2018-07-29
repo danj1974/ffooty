63775b59b09f77833de8decdec98118c963d6d6d
danj1974 <danj1974@gmail.com>
2018-07-29 21:52:16 +0100

    master: adding db location for python_anywhere deployment

--- ffooty/settings/python_anywhere.py
+++ ffooty/settings/python_anywhere.py
@@ -35,7 +35,7 @@ DEFAULT_FILE_STORAGE = 'storage.handlers.DatabaseStorage'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "data", "db.sqlite3"),
         "USER": "",
         "PASSWORD": "",
         "HOST": "",
