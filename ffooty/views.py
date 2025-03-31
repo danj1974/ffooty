afe7136c48588c6ca9febecf6bea7b62add807fd
danj1974 <danj1974@gmail.com>
2025-03-31 21:05:04 +0100

    Adds an IntegerField for week on the player upload page.

--- ffooty/views.py
+++ ffooty/views.py
@@ -118,7 +118,7 @@ class PlayerUpdateFileUploadView(LoginRequiredMixin, TemplateView):
 
         if form.is_valid():
             uploaded_file = request.FILES['file']
-            week = Week.objects.filter(number=form.week).first() or get_week()
+            week = Week.objects.filter(number=form.data['week']).first() or get_week()
             update_players_json(week, file_object=uploaded_file)
             update_weekly_scores(week)
 
